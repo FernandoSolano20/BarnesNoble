9619c13d4095503f48d0bdf0efff1cc16021301e
fesolano <fesolano@outcoding.com>
2019-07-13 21:11:38 -0600

    Arreglar footer problema, terminar el crud de genero y solucionar bugs de genero a la hora de crear, editar, eliminar o activar y desactivar.

--- api/routes/genero.js
+++ api/routes/genero.js
@@ -4,41 +4,42 @@ const express = require('express'),
     router = express.Router(),
     Genero = require('../models/genero.model');
 
-router.post('/registrarGenero', function(req, res){
+router.post('/registrarGenero', function (req, res) {
     let body = req.body;
-    let nuevoGenero =  new Genero({
+    let nuevoGenero = new Genero({
         nombre: body.nombre,
         descripcion: body.descripcion,
         estado: body.estado
     });
 
     nuevoGenero.save(
-        function(err,contactoDB){
-            if(err){
+        function (err, genero) {
+            if (err) {
                 return res.status(400).json({
                     success: false,
-                    msj: 'El género no se pudo guardar',
+                    message: 'El género no se pudo guardar',
                     err
                 });
-            }else{
+            } else {
                 return res.json({
                     success: true,
-                    msj: 'El género se guardó con éxito'
+                    genero: genero,
+                    message: 'El género se guardó con éxito'
                 })
             }
         }
     );
 });
 
-router.get('/listarGeneros', function(req, res){
-    Genero.find(function(err, generosBD){
-        if(err){
+router.get('/listarGeneros', function (req, res) {
+    Genero.find(function (err, generosBD) {
+        if (err) {
             return res.status(400).json({
                 success: false,
                 msj: 'No se pueden listar los géneros',
                 err
             });
-        }else{
+        } else {
             return res.json({
                 success: true,
                 listaGeneros: generosBD
@@ -47,24 +48,76 @@ router.get('/listarGeneros', function(req, res){
     });
 });
 
-router.put('/editar/:id', function(req, res){
-    Genero.findByIdAndUpdate(req.params.id, { $set: req.body }, function (err, genero) {
-        if (err) return res.status(500).send(err);
-        const response = {
-            message: "Genero actualizado",
-            genero: genero
-        };
-        return res.status(200).json({response});
+router.put('/editar/:id', function (req, res) {
+    Genero.findByIdAndUpdate(req.params.id, { $set: req.body }, function (err) {
+        if (err) {
+            return res.status(400).json({
+                success: false,
+                message: 'El género no se pudo editar',
+                err
+            });
+        }
+        Genero.findById(req.params.id, (err, genero) => {
+            return res.status(200).json({
+                success: true,
+                message: "Genero editado",
+                genero: genero
+            })
+        });
     });
 });
 
-router.delete('/eliminar/:id', function(req, res){
+router.delete('/eliminar/:id', function (req, res) {
     Genero.findByIdAndRemove(req.params.id, function (err) {
-        if (err) return res.status(500).send(err);
-        const response = {
-            message: "Genero elimnado",
-        };
-        return res.status(200).json({response});
+        if (err) {
+            return res.status(400).json({
+                success: false,
+                message: 'El género no se pudo eliminar',
+                err
+            });
+        }
+        return res.status(200).json({
+            success: true,
+            message: "Genero elimnado"
+        });
+    });
+});
+
+router.patch('/modificarEstado/:id', function (req, res) {
+    Genero.findById(req.params.id, (err, genero) => {
+        if (err) {
+            return res.status(400).json({
+                success: false,
+                message: 'No se pudo cambiar el estado del género',
+                err
+            });
+        }
+
+        genero.set(req.body);
+
+        genero.save((err, generosBD) => {
+            if (err)
+                return res.status(400).json({
+                    success: false,
+                    message: 'No se pudo cambiar el estado del género',
+                    err
+                });
+            let response;
+            if (req.body.estado) {
+                response = {
+                    success: true,
+                    message: "Genero activado",
+                    genero: generosBD
+                };
+            } else {
+                response = {
+                    success: true,
+                    message: "Genero desactivado",
+                    genero: generosBD
+                };
+            }
+            return res.status(200).json({ response });
+        });
     });
 });
 
--- public/js/genero/servicioGenero.js
+++ public/js/genero/servicioGenero.js
@@ -1,3 +1,15 @@
+var crearGenero = async (genero) => {
+    var response = await fetch('http://localhost:4000/api/genero/registrarGenero', {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json;charset=UTF-8',
+        },
+        body:JSON.stringify(genero)
+    });
+    var result = await response.json();
+    return result;
+}
+
 var obtenerGenero = async () => {
     var response = await fetch('http://localhost:4000/api/genero/listarGeneros', {
         method: "GET",
@@ -18,7 +30,7 @@ var editarGenero = async(genero,id) => {
         body:JSON.stringify(genero)
     });
     var result = await response.json();
-    return result.response;
+    return result;
 }
 
 var eliminarGenero = async(id) => {
@@ -27,6 +39,18 @@ var eliminarGenero = async(id) => {
         headers: {
             'Content-Type': 'application/json;charset=UTF-8',
         },
+    });
+    var result = await response.json();
+    return result;
+}
+
+var estadoGenero = async(genero,id) => {
+    var response = await fetch('http://localhost:4000/api/genero/modificarEstado/' + id, {
+        method: "PATCH",
+        headers: {
+            'Content-Type': 'application/json;charset=UTF-8',
+        },
+        body:JSON.stringify(genero)
     });
     var result = await response.json();
     return result.response;
