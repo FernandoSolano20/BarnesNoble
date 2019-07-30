5a1e536d84012a1b2f2fae902131ec4cac97dcd3
fesolano <fesolano@outcoding.com>
2019-07-29 20:35:34 -0600

    Apuntar perfil libreia lista libreria

--- api/routes/libreria.js
+++ api/routes/libreria.js
@@ -64,27 +64,3 @@ router.get('/sucursalId/:id', function (req, res) {
 });
  
 module.exports = router;
-
-//localhosta:3000/api/registrarUsuario/
-//localhosta:3000/api/registrarTarjeta/
-
-  // nuevoUsuario.save(   
-    //     function (err, usuarioDB) {
-    //         if (err)
-    //             res.status(400).json({
-    //                 success: true,
-    //                 msj: 'El usario no se guardó con éxito' 
-    //             });
-
-
-
-                
-                
-            //Obtiene y devuelve todas las personas tras crear una de ellas
-    //         Usario.find(function (err, conct) {
-    //             if (err)
-    //                 res.send(err)
-    //             res.json(conct);
-    //         })
-    //     }
-    // );
