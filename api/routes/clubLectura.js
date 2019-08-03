3255eb7ec227129159fdcd4dfcf9662c197d648b
sebarks <svallejos@ucenfotec.ac.cr>
2019-08-02 20:32:52 -0600

    cree el registro de club de lectura

--- api/routes/clubLectura.js
+++ api/routes/clubLectura.js
@@ -4,25 +4,30 @@ const express = require('express'),
     router = express.Router(),
     ClubLectura = require('../models/clubLectura.model');
 
+router.param('_id'), function (req, res, next, _id) {
+    req.body._id = _id;
+    next();
+}
+
 //Definición de la ruta para registrar contactos
 
-router.post('/registrarClubLectura', function(req, res) {
+router.post('/registrarClubLectura', function (req, res) {
     let body = req.body;
 
     let nuevoClubLectura = new ClubLectura({
         nombre: body.nombre,
-        tema:body.tema,
+        tema: body.tema,
         tipoClub: body.tipoClub,
         fechaReunion: body.fechaReunion,
         horaReunion: body.horaReunion,
-        sucursal:body.sucursal,
-        administrador:body.administrador,
-        categoria:body.categoria,
-        genero:body.genero
+        sucursal: body.sucursal,
+        administrador: body.administrador,
+        categoria: body.categoria,
+        genero: body.genero
     });
 
     nuevoClubLectura.save(
-        function(err, clubesLecturaDB) {
+        function (err, clubesLecturaDB) {
             if (err) {
                 return res.status(400).json({
                     success: false,
@@ -39,8 +44,8 @@ router.post('/registrarClubLectura', function(req, res) {
     );
 });
 
-router.get('/listarClubLectura', function(req, res) {
-    ClubLectura.find(function(err, clubesLecturaBD) {
+router.get('/listarClubLectura', function (req, res) {
+    ClubLectura.find(function (err, clubesLecturaBD) {
         if (err) {
             return res.status(400).json({
                 success: false,
@@ -57,3 +62,23 @@ router.get('/listarClubLectura', function(req, res) {
 });
 
 module.exports = router;
+
+
+router.get('/usuarioId/:id', async (req, res) => {
+    return await Usuario.findById(req.params.id, function (err, usuario) {
+        if (err) {
+            return res.status(400).json({
+                success: false,
+                message: 'No se encontro ninguna usuario',
+                err
+            });
+        }
+        else {
+            return res.json({
+                success: true,
+                usuario: usuario
+            });
+        }
+    })
+     
+});
--- public/js/clubLectura/controladorRegistrarClubLectura.js
+++ public/js/clubLectura/controladorRegistrarClubLectura.js
@@ -1,162 +1,80 @@
-let regexText = /^[a-zA-ZñÑáéíóúÁÉÍÓÚ\s]+(\s*[a-zA-ZñÑáéíóúÁÉÍÓÚ\s]*)*[a-zA-ZñÑáéíóúÁÉÍÓÚ\s]+$/;
-
-const nombreInput1 = document.getElementById('nombre-input');
-const nombreAlert1 = document.getElementById('alert-nombre1');
+const nombreInput = document.getElementById('nombre-input');
+const nombreAlert = document.getElementById('alert-nombre');
 
 const temaInput = document.getElementById('tema-input');
 const temaAlert = document.getElementById('alert-tema');
 
-const tipoInput = document.querySelectorAll('[name="tipo"]');
-const tipoAlert = document.getElementById('alert-tipo');
+let   tipoClubInput  = document.querySelectorAll('[name="tipoClub"]')
+const tipoClubAlert = document.getElementById('alert-tipo-club');
 
-const diaInput = document.getElementById('dia-input');
-const diaAlert = document.getElementById('alert-dia');
+const fechaReunionInput = document.getElementById('fecha-reunion-input');
+const fechaReunionAlert = document.getElementById('alert-fecha-reunion');
 
-const horaInput = document.getElementById('hora-input');
-const horalert = document.getElementById('alert-hora');
 
-const imgInput = document.getElementById('img');
-const imgAlert = document.getElementById('alert-img');
+const horaReunionInput = document.getElementById('hora-reunion-input');
+const horaReunionAlert = document.getElementById('alert-hora-reunion');
 
-const favAlert = document.getElementById('alert-favorito');
 
-let obtenerDatosClubLectura = async function () {
-    let error = validarNombre1() | validarTema() | validarTipo();
+let validarDatosClubLectura = async function () {
+    let error = validarNombre() | validarTema() | validarTipoClub()  | validarFechaReunion() | validarHoraReunion();
     if (!error) {
 
-        document.body.className = "loading";
-        let imgResult = await crearImagen(imgValue);
-        if (imgResult.success) {
-    
-            let clubLectura = {
-                nombre: nombreInput1.value,
-                tema: temaInput.value,
-                tipo: tipoValue,
-                dia: diaValue,
-                img: imgResult.result.secure_url,
-                hora: horaInput.value,
-                sucursal: ''
-            }
-            let nuevoClubLectura = await crearClubLectura(clubLectura);
-            document.body.className = "";
-            if (nuevoClubLectura.success) {
-                Swal.fire({
-                    type: 'success',
-                    title: nuevoClubLectura.message,
-                    showCloseButton: true,
-                    focusConfirm: false,
-                    confirmButtonText:
-                        '<a href="http://localhost:3000/inicioSesion.html" class="linkPage">Ok</a>'
-                });
-            }
-            else {
-                Swal.fire({
-                    type: 'error',
-                    title: nuevoClubLectura.message
-                });
-            }
+        let nombre = nombreInput.value;
+        let tema = temaInput.value;
+        for (let i = 0; i < tipoClubInput.length; i++){
+        if (tipoClubInput[i].checked){
+        tipoClubInput=tipoClubInput[i].value;
+        break;
         }
-        else {
-            document.body.className = "";
+    }
+        let fechaReunion = fechaReunionInput.value;
+        let horaReunion =horaReunionInput.value;
+
+        let clubLectura = {
+            nombre: nombre,
+            tema: tema,
+            tipoClub: tipoClubInput,
+            fechaReunion: fechaReunion,
+            horaReunion: horaReunion,
+            administrador: sessionStorage.id
+        }
+
+        let nuevoClubLectura = await registrarClubLectura(clubLectura);
+
+        if (nuevoClubLectura.success) {
             Swal.fire({
-                type: 'error',
-                title: imgResult.message
-            });
+                title: nuevoClubLectura.message,
+                type: 'success',
+                text: 'Se tarjeta se registró correctamente'
+
+            })
+            redirect();
+        } else {
+            Swal.fire({
+                title: nuevoClubLectura.message,
+                type: 'error'
+            })
         }
     }
     else {
         Swal.fire({
+            title: 'No se ha realizado el registro',
             type: 'warning',
-            title: 'No se ha enviado su mensaje exitosamente',
-            text: 'Revise los campos resaltados e intételo de nuevo'
-        });
+            text: 'Revise los campos resaltados e inténtelo de nuevo'
+        })
     }
-}
-
-let noVacio = function (elementos) {
-    if (elementos.value == "") {
-        elementos.alert.innerText = "Rellene el campo."
-        elementos.alert.className = elementos.alert.className.replace("alertHidden", "");
-        elementos.input.className = elementos.input.className.replace("inputError", "");
-        elementos.input.className = elementos.input.className + " inputError";
-        return false;
-    }
-    elementos.alert.className = elementos.alert.className.replace("alertHidden", "");
-    elementos.alert.className = elementos.alert.className + " alertHidden";
-    elementos.input.className = elementos.input.className.replace("inputError", "");
-    return true;
-}
-
-let validarRadio = function (elementos) {
-    for (let i = 0; i < elementos.input.length; i++) {
-        if (elementos.input[i].checked) {
-            elementos.alert.className = elementos.alert.className.replace("alertHidden", "");
-            elementos.alert.className = elementos.alert.className + " alertHidden";
-            return true;
-        }
-    }
-    elementos.alert.innerText = "Campo requerido."
-    elementos.alert.className = elementos.alert.className.replace("alertHidden", "");
-    elementos.input[0].parentElement.className = elementos.input[0].parentElement.className + " errorRadio";
-    return false;
-}
-
-
-let cambiarTipo = function () {
-    for (let i = 0; i < idRadios.length; i++) {
-        if (idRadios[i].checked) {
-            document.getElementById('labelID').innerHTML = idRadios[i].value;
-            idInput.name = idRadios[i].value;
-        
-        }
-    }
-    validartipo();
-}
-
-let validarFotoPerfil = function () {
-    let elementPicture = {
-        value: imgInput.value,
-        alert: imgAlert,
-        input: imgInput
-    }
-    return !(noVacio(elementPicture) && validarFotos(elementPicture));
-}
-
-
-let validarTexto = function (elementos) {
-    if (!regexText.test(elementos.value)) {
-        elementos.alert.innerText = "Solo debe tener letras."
-        elementos.alert.className = elementos.alert.className.replace("alertHidden", "");
-        elementos.input.className = elementos.input.className.replace("inputError", "");
-        elementos.input.className = elementos.input.className + " inputError";
-        return false;
-    }
-    elementos.alert.className = elementos.alert.className.replace("alertHidden", "");
-    elementos.alert.className = elementos.alert.className + " alertHidden";
-    elementos.input.className = elementos.input.className.replace("inputError", "");
-    return true;
-}
+};
 
 
 
-let validarNombre1 = function () {
+let validarNombre = function () {
     let elementText = {
-        value: nombreInput1.value,
-        alert: nombreAlert1,
-        input: nombreInput1
+        value: nombreInput.value,
+        alert: nombreAlert,
+        input: nombreInput
     }
     return !(noVacio(elementText) && validarTexto(elementText));
 }
-
-let validarHora = function () {
-    let elementTime = {
-        value: horaInput.value,
-        alert: horaAlert,
-        input: horaInput
-    }
-    return !(noVacio(elementTime) && validarTexto(elementTime));
-}
-
 
 let validarTema = function () {
     let elementText = {
@@ -164,31 +82,39 @@ let validarTema = function () {
         alert: temaAlert,
         input: temaInput
     }
-    return !(noVacio(elementText) && validarTexto(elementText));
+ return !(noVacio(elementText) && validarTexto(elementText));
 }
 
-
-let validarTipo = function () {
-    let elementCheckbox = {
-        alert: tipoAlert,
-        input: tipoInput
+let validarFechaReunion = function () {
+    let elementSelect = {
+        value: fechaReunionInput.value,
+        alert: fechaReunionAlert,
+        input: fechaReunionInput
     }
-    if (!validarRadio(elementCheckbox)) {
-        return true;
-    }
-    else {
-        tipoAlert.className = tipoAlert.className.replace("alertHidden", "");
-        tipoAlert.className = tipoAlert.className + " alertHidden";
-        tipoInput[0].parentElement.className = tipoInput[0].parentElement.className.replace("errorRadio", "");
-        return false;
-    }
+    return !(validarSelect(elementSelect));
 }
 
+let validarTipoClub = function () {
+    let elementRadio = {
+        alert: tipoClubAlert,
+        input: tipoClubInput
+    }
+    return !(validarRadio(elementRadio));
+}
 
-nombreInput1.addEventListener('blur', validarNombre1);
+let validarHoraReunion = function () {
+    let elementTime = {
+        value: fechaReunionInput.value,
+        alert: fechaReunionAlert,
+        input: fechaReunionInput
+    }
+    return !(noVacio(elementTime));
+}
+
+nombreInput.addEventListener('blur', validarNombre);
 temaInput.addEventListener('blur', validarTema);
-imgInput.addEventListener('change', validarFotoPerfil);
-for (let i = 0; i < tipoInput.length; i++)
-    tipoInput[i].addEventListener('change', validarTipo);
-
-document.getElementById('registrar').addEventListener('click', obtenerDatosClubLectura);
+for (let i = 0; i < tipoClubInput.length; i++)
+tipoClubInput[i].addEventListener('change', validarTipoClub);
+fechaReunionInput.addEventListener('change', validarFechaReunion);
+horaReunionInput.addEventListener('change', validarHoraReunion);
+document.getElementById('registrar').addEventListener('click', validarDatosClubLectura);
