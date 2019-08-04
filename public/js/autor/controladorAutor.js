4fbf48ccaf7e3763b063fef6af2b0a9ac1490e91
luisfersol20@gmail.com <luisfersol20@gmail.com>
2019-08-04 08:30:27 -0600

    Autor error

--- public/js/autor/controladorAutor.js
+++ public/js/autor/controladorAutor.js
@@ -37,32 +37,7 @@ let agregarFilaAutores = function (autor) {
     let fila = tbody.insertRow();
     fila.setAttribute('data-id', autor._id);
     fila.insertCell().innerHTML = autor.nombre;
-
-    let editarCelda = fila.insertCell();
-    let editar = document.createElement('i');
-    editar.setAttribute('class', 'far fa-edit');
-    editar.setAttribute('data-id', autor._id);
-    editarCelda.appendChild(editar);
-
-    let eliminarCelda = fila.insertCell();
-    let eliminar = document.createElement('i');
-    eliminar.setAttribute('class', 'fal fa-trash-alt');
-    eliminar.setAttribute('data-action', 'borrar');
-    eliminarCelda.appendChild(eliminar);
-
-    let estadoCelda = fila.insertCell();
-
-    let estadoInput = document.createElement('input');
-    estadoInput.setAttribute('class', 'switch');
-    estadoInput.setAttribute('id', autor._id);
-    estadoInput.setAttribute('type', 'checkbox');
-    estadoCelda.appendChild(estadoInput);
-    estadoInput.checked = !autor.estado;
-
-    let estadoLabel = document.createElement('label');
-    estadoLabel.setAttribute('data-action', 'estado');
-    estadoLabel.setAttribute('for', autor._id);
-    estadoCelda.appendChild(estadoLabel);
+    
     let premios = fila.insertCell();
     let btnPremios = document.createElement('a');
     btnPremios.innerText = 'Agregar Premios';
