9723bd3c969a2dc4e60ed793b054bb24cec8900c
luisfersol20@gmail.com <luisfersol20@gmail.com>
2019-08-04 21:51:01 -0600

    Try catch en imagenes

--- api/routes/imagenes.js
+++ api/routes/imagenes.js
@@ -4,20 +4,25 @@ const cloudinary = require('cloudinary').v2,
 
 router.post('/crear', async (req, res) => {
     const file = req.files.photo;
-    return await cloudinary.uploader.upload(file.tempFilePath, function (err, result) {
-        if (err) {
-            return res.status(400).json({
-                success: false,
-                message: 'La imagen no se pudo almacenar',
-                err
-            });
-        } else {
-            return res.json({
-                success: true,
-                result: result
-            })
-        }
-    });
+    try {
+        return await cloudinary.uploader.upload(file.tempFilePath, function (err, result) {
+            if (err) {
+                return res.status(400).json({
+                    success: false,
+                    message: 'La imagen no se pudo almacenar',
+                    err
+                });
+            } else {
+                return res.json({
+                    success: true,
+                    result: result
+                })
+            }
+        });
+    }catch(e){
+        console.log("Error" + e);
+    }
+    
 });
 
 module.exports = router;
